4c96b8aa3a1c50726d48b719a62fd55d60cd6cab
Kaushal Chakardhari <kaushal000126@gmail.com>
2020-09-01 10:18:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+alert("hello");
+// document.querySelector("button").addEventListener("click", handleclick);
+//fuction handleclick()
+//{
+//  alert("I got clicked!");
+//}
+var n = document.querySelectorAll(".drum").length;
+//var m= Math.floor(Math.random()*4)+1;
+              // for pressing button
+for(var i=0;i<n;i++)
+{
+document.querySelectorAll("button")[i].addEventListener("click",function (){
+
+//var buttonInnerHTML = this.innerHTML;
+  addSound(this.innerHTML);
+ buttonAnimation(this.innerHTML);
+
+});
+}
+             // for pressing selected keys
+document.addEventListener("keypress",function(event){
+//  var key = event.press;
+addSound(event.key);
+ buttonAnimation(event.key);
+});
+
+function addSound(key){
+  switch(key)
+  {
+
+    case "w":
+    var ton1 = new Audio("sounds/crash.mp3");
+    ton1.play();
+    break;
+
+    case "a":
+    var ton2 = new Audio("sounds/kick-bass.mp3");
+    ton2.play();
+    break;
+
+    case "s":
+    var ton3 = new Audio("sounds/snare.mp3");
+    ton3.play();
+    break;
+
+    case "d":
+    var ton4 = new Audio("sounds/tom-1.mp3");
+    ton4.play();
+    break;
+
+    case "j":
+    var ton5 = new Audio("sounds/tom-2.mp3");
+    ton5.play();
+    break;
+    case "k":
+    var ton7 = new Audio("sounds/tom-3.mp3");
+    ton7.play();
+    break;
+    case "l":
+    var ton6 = new Audio("sounds/tom-4.mp3");
+    ton6.play();
+    break;
+    default: console.log(key);
+
+  }
+
+}
+function buttonAnimation(currentKey){
+  var activeButton=document.querySelector("."+ currentKey);
+  activeButtton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed")},100);
+}
